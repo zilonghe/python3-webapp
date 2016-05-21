8b12e986fa19ad93478734a93095bc28e3673dac
zilonghe <495309675@qq.com>
2016-05-22 01:16:34 +0800

    add pymoniter
    auto restart the whole service whenever it’s modified.

--- www/handlers.py
+++ www/handlers.py
@@ -106,7 +106,7 @@ def cookie2user(cookie_str):
 def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='1', name='Test Blog2', summary=summary, created_at=time.time()-120),
         Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
         Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
     ]
